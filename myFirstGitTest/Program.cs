c01da2a310ba639b1d476ebc32a33ea89fd46f1a
DESKTOP-ALLBLN9\Ben <bvjanis@sbcglobal.net>
2018-07-21 01:24:43 -0500

    my first checkin

---
+++ myFirstGitTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myFirstGitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first GIT test, version 1");
+            Console.ReadKey();
+        }
+    }
+}
